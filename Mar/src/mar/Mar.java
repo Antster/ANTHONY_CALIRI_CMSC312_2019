142d42017f194795cc04b9f373444d7d468e62a0
caliriap <caliriap@vcu.edu>
2019-09-30 15:55:43 -0400

    - created empty Java project - Mar

---
+++ Mar/src/mar/Mar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mar;
+
+/**
+ *
+ * @author Anthony Caliri
+ */
+public class Mar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
