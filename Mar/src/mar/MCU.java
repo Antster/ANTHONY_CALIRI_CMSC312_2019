e5ede5494a43c6842d4d036ad4f1e6a0ad001869
caliriap <43229705+caliriap@users.noreply.github.com>
2019-11-19 21:50:51 -0500

    - pulled all main memory components into MCU class

---
+++ Mar/src/mar/MCU.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tonyt
+ */
+public class MCU {
+
+    private static int memorySpace;
+    
+    private static List<PCB> memoryList = new ArrayList<>();
+    
+    public MCU(int memSpace) {
+        this.memorySpace = memSpace;
+    }
+    
+    protected static int getMemorySpace() {
+        return memorySpace;
+    }
+    
+    protected static void setMemorySpace(int in){
+        memorySpace = in;
+    }
+    
+    protected static void addToMemList(PCB p){
+        memoryList.add(p);
+    }
+    protected static void removeToMemList(PCB p){
+        memoryList.remove(p);
+    }
+    
+    protected static boolean memHasSpace(int in) {
+        return (memorySpace - in) >= 0;
+    }
+}
